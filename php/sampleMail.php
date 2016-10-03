499dc7450e0202514fcaa3b55524803e0d5965ec
Hardikgiri Goswami <hardikgiri.goswami@gmail.com>
2016-10-03 12:31:24 +0530

    [ADD]Added icon for favicon and also added email on contact form.

--- php/sampleMail.php
+++ php/sampleMail.php
@@ -1,7 +1,7 @@
 <?php
 	if($_SERVER['REQUEST_METHOD'] == "POST")
 	{
-		if(mail("youremail4@email.com", "New Sample User!", "User: " . $_POST["firstname"] . " Email: " . $_POST["email"], $headers))
+		if(mail("hardikgiri.goswami@gmail.com", "New Sample User!", "User: " . $_POST["firstname"] . " Email: " . $_POST["email"], $headers))
                      echo "You've been successfully subscribed!";
                 else
                      echo "There was a problem!";
